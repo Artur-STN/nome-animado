b429f4db202dae0340f69a238c07090378eb329b
Artur Santos <artur_teixeira10@hotmail.com>
2020-08-26 19:52:30 -0300

    Codigos

---
+++ script.js
@@ -0,0 +1,12 @@
+function dados() {
+    
+    let nome = document.getElementById('nome')
+    let ocupacao = document.getElementById('ocupacao')
+
+    let dado_nome = document.getElementById('i_nome')
+    let dado_ocupacao = document.getElementById('i_ocupacao')
+
+    nome.innerHTML = dado_nome.value
+    ocupacao.innerHTML = dado_ocupacao.value
+
+}
