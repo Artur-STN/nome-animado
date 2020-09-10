7121865ea25fe8153d3939baf4cf0e707f1ad4a6
Artur Santos <artur_teixeira10@hotmail.com>
2020-09-10 09:03:44 -0300

    a

--- script.js
+++ script.js
@@ -6,8 +6,28 @@ function pegar_dados() {
     let i_nome = document.getElementById('i_nome')
     let i_ocupacao = document.getElementById('i_ocupacao')
 
+    let botao = document.getElementById('botao_pegar')    
+    
     nome.innerHTML = i_nome.value
     ocupacao.innerHTML = i_ocupacao.value
+
+    botao.style.background = 'rgb(0, 36, 70)';
+
+}
+
+function voltar_bg() {
+    
+    let botao = document.getElementById('botao_pegar')
+
+    botao.style.background = 'rgb(0, 183, 255)'
+
+}
+
+function hover() {
+    
+    let botao = document.getElementById('botao_pegar')
+
+    botao.style.background = 'rgb(0, 131, 253)'
 
 }
 
@@ -18,6 +38,8 @@ function rodar_animacao() {
     let nome = document.querySelector('.nome-nome-animado')
     let ocupacao = document.querySelector('.ocupacao-nome-animado')
 
+    caixa.style.visibility = ''
+
     caixa.style.visibility = 'visible'
     img.style.animation = 'anima 1.5s'
     nome.style.animation = 'anima 0.5s'
@@ -27,7 +49,6 @@ function rodar_animacao() {
 
 function fechar_animacao() {
 
-    let caixa = document.querySelector('.nome-animado')
     let img = document.querySelector('.imagem-nome-animado')
     let nome = document.querySelector('.nome-nome-animado')
     let ocupacao = document.querySelector('.ocupacao-nome-animado')
